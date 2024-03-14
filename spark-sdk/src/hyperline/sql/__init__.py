09da24de49f3831c5bbc30a962db63ced776c892
sahilsync <105463588+sahilsync@users.noreply.github.com>
2024-03-14 15:22:14 -0700

    execute sql api 1

---
+++ spark-sdk/src/hyperline/sql/__init__.py
@@ -0,0 +1,3 @@
+from ._api_trino import *
+
+
---
+++ spark-sdk/src/hyperline/sql/_api_trino.py
@@ -0,0 +1,34 @@
+"""
+Primitives to create Pandas Dataframes represending crypto datasets using Hyperline Trino.
+"""
+from trino.dbapi import connect
+import pandas as pd
+
+__all__ = [
+    "execute_sql",
+]
+
+def execute_sql(
+  query: str,
+) -> pd.DataFrame:
+  host = '10.138.0.11'
+  port = "8080"
+  user = 'ws-demo3@mvp-infra.iam.gserviceaccount.com'
+  catalog="hyperlake"
+  schema="hyperdata"
+
+  conn = connect(
+      host=host,
+      port=port,
+      user=user,
+      catalog=catalog,
+      schema=schema,
+  )
+  cur = conn.cursor()
+
+  cur.execute(query)
+  rows = cur.fetchall()
+  df = pd.DataFrame(rows)
+  return df
+
+
